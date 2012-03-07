32ef64b2b44d9a5077b11bf8d8e0b17015bd305a
Kervala <983521+kervala@users.noreply.github.com>
2012-03-07 21:36:05 +0100

    Added: New CMake modules

---
+++ modules/FindGCrypt.cmake
@@ -0,0 +1,1 @@
+FIND_PACKAGE_HELPER(GCrypt gcrypt.h "gcrypt libgcrypt gcrypt-11 libgcrypt-11" "gcryptd libgcryptd gcrypt-11d libgcrypt-11d")
---
+++ modules/FindGnuTLS.cmake
@@ -0,0 +1,1 @@
+FIND_PACKAGE_HELPER(GnuTLS gnutls/gnutls.h "gnutls libgnutls gnutls-26 libgnutls-26" "gnutlsd libgnutlsd gnutls-26d libgnutls-26d")
