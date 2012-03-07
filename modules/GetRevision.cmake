5c51c44e8f067452cc94447318c7a77de9177ec6
Kervala <983521+kervala@users.noreply.github.com>
2012-03-07 21:32:54 +0100

    Changed: Define correct BUILD_DATE

--- modules/GetRevision.cmake
+++ modules/GetRevision.cmake
@@ -1,26 +1,52 @@
 CMAKE_MINIMUM_REQUIRED(VERSION 2.6.3)
 
+# ROOT_DIR should be set to root of the repository (where to find the .svn or .hg directory)
+# SOURCE_DIR should be set to root of your code (where to find CMakeLists.txt)
+
 SET(CMAKE_MODULE_PATH "${SOURCE_DIR}/CMakeModules;${CMAKE_MODULE_PATH}")
 
-IF(EXISTS "${SOURCE_DIR}/.svn/")
+IF(NOT ROOT_DIR AND SOURCE_DIR)
+  SET(ROOT_DIR ${SOURCE_DIR})
+ENDIF(NOT ROOT_DIR AND SOURCE_DIR)
+
+IF(NOT SOURCE_DIR AND ROOT_DIR)
+  SET(SOURCE_DIR ${ROOT_DIR})
+ENDIF(NOT SOURCE_DIR AND ROOT_DIR)
+
+MACRO(NOW RESULT)
+  IF (WIN32)
+    EXECUTE_PROCESS(COMMAND "wmic" "os" "get" "localdatetime" OUTPUT_VARIABLE DATETIME)
+    STRING(REGEX REPLACE ".*\n([0-9][0-9][0-9][0-9])([0-9][0-9])([0-9][0-9])([0-9][0-9])([0-9][0-9])([0-9][0-9]).*" "\\1-\\2-\\3 \\4:\\5:\\6" ${RESULT} ${DATETIME})
+  ELSEIF(UNIX)
+    EXECUTE_PROCESS(COMMAND "date" "+'%Y-%m-%d %H:%M:%S'" OUTPUT_VARIABLE ${RESULT})
+  ELSE (WIN32)
+    MESSAGE(SEND_ERROR "date not implemented")
+    SET(${RESULT} 000000)
+  ENDIF (WIN32)
+ENDMACRO(NOW)
+
+IF(EXISTS "${ROOT_DIR}/.svn/")
   FIND_PACKAGE(Subversion)
 
   IF(SUBVERSION_FOUND)
-    Subversion_WC_INFO(${SOURCE_DIR} ER)
+    Subversion_WC_INFO(${ROOT_DIR} ER)
     SET(REVISION ${ER_WC_REVISION})
   ENDIF(SUBVERSION_FOUND)
-ENDIF(EXISTS "${SOURCE_DIR}/.svn/")
+ENDIF(EXISTS "${ROOT_DIR}/.svn/")
 
-IF(EXISTS "${SOURCE_DIR}/.hg/")
+IF(EXISTS "${ROOT_DIR}/.hg/")
   FIND_PACKAGE(Mercurial)
 
   IF(MERCURIAL_FOUND)
-    Mercurial_WC_INFO(${SOURCE_DIR} ER)
+    Mercurial_WC_INFO(${ROOT_DIR} ER)
     SET(REVISION ${ER_WC_REVISION})
   ENDIF(MERCURIAL_FOUND)
-ENDIF(EXISTS "${SOURCE_DIR}/.hg/")
+ENDIF(EXISTS "${ROOT_DIR}/.hg/")
 
-IF(REVISION AND EXISTS ${SOURCE_DIR}/revision.h.in)
-  CONFIGURE_FILE(${SOURCE_DIR}/revision.h.in revision.h.txt)
-  EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E copy revision.h.txt revision.h) # copy_if_different
-ENDIF(REVISION AND EXISTS ${SOURCE_DIR}/revision.h.in)
+IF(REVISION)
+  IF(EXISTS ${SOURCE_DIR}/revision.h.in)
+    NOW(BUILD_DATE)
+    CONFIGURE_FILE(${SOURCE_DIR}/revision.h.in revision.h.txt)
+    EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E copy revision.h.txt revision.h) # copy_if_different
+  ENDIF(EXISTS ${SOURCE_DIR}/revision.h.in)
+ENDIF(REVISION)
