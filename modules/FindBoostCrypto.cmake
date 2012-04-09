7572b109945102b76dc7b9385f0e0b1ec8897e37
Kervala <983521+kervala@users.noreply.github.com>
2012-04-09 11:12:56 +0200

    Fixed: Multiarch and compiling both static and shared libraries

--- modules/FindBoostCrypto.cmake
+++ modules/FindBoostCrypto.cmake
@@ -2,4 +2,4 @@ IF(CRYPTO_DIR)
   SET(BOOSTCRYPTO_DIR ${CRYPTO_DIR})
 ENDIF(CRYPTO_DIR)
 
-FIND_PACKAGE_HELPER(BoostCrypto crypto/md5.hpp "boost_crypto crypto" "boost_cryptod cryptod")
+FIND_PACKAGE_HELPER(BoostCrypto crypto/md5.hpp boost_crypto boost_cryptod)
