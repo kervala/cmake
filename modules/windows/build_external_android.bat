39f1838f2704d8e803e2ad74561ce94aba3a228e
Kervala <983521+kervala@users.noreply.github.com>
2013-10-18 10:31:07 +0200

    Fixed: Android externals compilation

--- modules/windows/build_external_android.bat
+++ modules/windows/build_external_android.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PATH=%PATH%;%NDK_ROOT%\prebuilt\windows-x86_64\bin;%NDK_ROOT%\prebuilt\windows\bin
+set PATH=%NDK_ROOT%\prebuilt\windows-x86_64\bin;%NDK_ROOT%\prebuilt\windows\bin;%NDK_ROOT%\toolchains\llvm-3.3\prebuilt\windows-x86_64\bin;%PATH%
 set EXTERNAL_ROOT=d:\External
 set TARGET_CPU=armv7
 
@@ -19,13 +19,22 @@ call:compile mcu
 echo All done.
 goto:eof
 
+:error
+set NAME=%~1
+echo An error occured while compiling %NAME%...
+pause
+goto:eof
+
 :compile
-echo Compiling %~1
-cd %EXTERNAL_ROOT%\%~1
+set NAME=%~1
+echo Compiling %NAME%
+cd %EXTERNAL_ROOT%\%NAME%
+rmdir /s /q build_android 2> nul
 mkdir build_android 2> nul
 cd build_android
 del CMakeCache.txt 2> nul
-cmake -G "Unix Makefiles" .. -DCMAKE_TOOLCHAIN_FILE=%CMAKE_MODULE_PATH%/AndroidToolChain.cmake -DCMAKE_INSTALL_PREFIX=%EXTERNAL_ANDROID_PATH% -DTARGET_CPU=%TARGET_CPU%
+cmake -G "Unix Makefiles" .. -DCMAKE_TOOLCHAIN_FILE=%CMAKE_MODULE_PATH%/AndroidToolChain.cmake -DCMAKE_INSTALL_PREFIX=%EXTERNAL_ANDROID_PATH% -DTARGET_CPU=%TARGET_CPU% -DCMAKE_BUILD_TYPE=Release -DNDK_TOOLCHAIN_VERSION=clang
+if ERRORLEVEL 1 call:error %NAME%
 make -j4
 make install
 goto:eof
